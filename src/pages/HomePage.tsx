76c5fae136dff516035998e950aecd565aec98ed
DenisaCazacu <denisa.cazacu01@gmail.com>
2025-04-12 23:20:26 +0300

    landing page version 1 (incomplete)
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	public/logo.png
    #	src/components/CardsSection.tsx
    #	src/components/LandingSection.tsx
    #	src/components/Navbar.tsx
    #	src/components/ui/Container.tsx
    #	src/components/ui/EventCard.tsx
    #	src/pages/index.tsx
    #	src/styles/Button.module.css
    #	src/styles/CardsSectionStyle.module.css
    #	src/styles/Container.module.css
    #	src/styles/EventCardStyle.module.css
    #	src/styles/HomePageStyle.module.css
    #	src/styles/LandingPageStyle.module.css
    #	src/styles/LandingSectionStyle.module.css
    #	src/styles/Navbar.module.css
    #	src/styles/globals.css

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './../styles/HomePageStyle.module.css'
+import Navbar from '../components/Navbar';
+import Container from "../components/ui/Container";
+import ButtonPrimary from "../components/ui/ButtonPrimary";
+import ButtonSecondary from "../components/ui/ButtonSecondary";
+
+export default function HomePage() {
+  return (
+    <>
+    <Navbar/>
+          <main>
+            <Container>
+              <h1>Title</h1>
+              <div className={styles["subcontainer-showcase"]}></div>
+              <div className={styles["buttons-showcase"]}>
+                <ButtonPrimary/>
+                <ButtonSecondary/>
+              </div>
+            </Container>
+          </main>
+    </>
+  )
+}
