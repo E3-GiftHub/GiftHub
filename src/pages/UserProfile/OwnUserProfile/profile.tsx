8936c47832289e89875b47c4cb43706c2b64dd35
MaXCoX610 <154246298+Dan-works-on-stuff@users.noreply.github.com>
2025-04-16 22:09:29 +0300

    development started

---
+++ src/pages/UserProfile/OwnUserProfile/profile.tsx
@@ -0,0 +1,15 @@
+import styles from "~/styles/UserProfile/UserProfile.module.css";
+import Navbar from "~/components/Navbar";
+import UserProfileUI from "~/components/ui/UserProfile/UserProfileUI";
+
+export default function UserProfile() {
+  return (
+    <div className={styles['landing-page']}>
+      <Navbar />
+      <UserProfileUI />
+      <div className={styles['empty-space']}></div>
+    </div>
+  )
+}
+
+
