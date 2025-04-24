1c77a46d566f461bed14455a15a62395eed846fa
MaXCoX610 <154246298+Dan-works-on-stuff@users.noreply.github.com>
2025-04-25 01:25:56 +0300

    close to done

--- src/components/ui/UserProfile/UserProfileUI.tsx
+++ src/components/ui/UserProfile/UserProfileUI.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from 'src/styles/UserProfile/UserProfile.module.css';
+import Image from 'next/image';
 
 export default function UserProfileUI() {
   return (
@@ -7,11 +8,13 @@ export default function UserProfileUI() {
       <div className={styles.profileCard}>
         {/* Left side with avatar */}
         <div className={styles.avatarSection}>
-          <div className={styles.avatarCircle}>
+          <div className={styles.avatarWrapper}>
+            <div className={styles.avatarCircle}></div>
+            <button className={styles.editAvatarButton}></button>
           </div>
-          <button className={styles.editAvatarButton}>
-          </button>
         </div>
+
+
 
         {/* Right side with user info */}
         <div className={styles.userInfo}>
@@ -19,13 +22,27 @@ export default function UserProfileUI() {
           <p className={styles.email}>melodiepedouavoci@gmail.com</p>
 
           <div className={styles.buttonContainer}>
-            <button className={styles.deleteButton}>
-              <span className={styles.buttonIcon}>🗑</span>
+            <button className={styles.button}>
+              <Image
+                src="/UserImages/buttons/bomb-icon.svg" // Path relative to public directory
+                alt="Delete"
+                width={18}  // Required
+                height={18}
+                // Required
+                className={styles.icon}
+              />
               Delete account
             </button>
 
-            <button className={styles.editButton}>
-              <span className={styles.buttonIcon}>✏️</span>
+            <button className={styles.button}>
+              <Image
+                src="/UserImages/buttons/edit-icon.svg" // Path relative to public directory
+                alt="Edit"
+                width={18}  // Required
+                height={18}
+                // Required
+                className={styles.icon}
+              />
               Edit info
             </button>
           </div>
