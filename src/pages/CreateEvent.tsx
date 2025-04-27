6f857cd87b470b818e6b7805111714613c654641
Mazuru Alin <alin.mazuru@yahoo.com>
2025-04-27 16:51:58 +0300

    adaugare pagini Create & Edit

---
+++ src/pages/CreateEvent.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import styles from "../styles/CreateEditEvent.module.css"
+import Navbar from "./_components/Navbar"
+import buttonStyles from "../styles/Button.module.css";
+
+
+export default function Page() {
+    return (
+        <div className={styles.pageWrapper}>
+            <Navbar />
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <h2>Create Event</h2>
+                </div>
+
+                <div className={styles.wrapper}>
+                    <form className={styles.form}>
+                        <div className={styles.left}>
+                            <label>
+                                Name your event:
+                                <input type="text" placeholder="e.g. John's Birthday Bash" />
+                            </label>
+                            <label className={styles.dateInputWrapper}>
+                                Set the date:
+                                <input type="date" />
+                                <span className={styles.icon}></span>
+                            </label>
+                            <label>
+                                Set the location:
+                                <input type="text" placeholder="Enter location" />
+                            </label>
+                        </div>
+
+                        <div className={styles.right}>
+                            <label>
+                                Add a note:
+                                <textarea placeholder="Event description..." />
+                            </label>
+                            <div className={styles.buttons}>
+                                <button className={`${buttonStyles.button} ${buttonStyles["button-primary"]}`}>
+                                    Cancel
+                                </button>
+
+                                <button className={`${buttonStyles.button} ${buttonStyles["button-primary"]}`}>
+                                    Save &  <br /> Continue
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/EditEvent.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import styles from "../styles/CreateEditEvent.module.css"
+import Navbar from "./_components/Navbar"
+import buttonStyles from "../styles/Button.module.css";
+
+export default function CreateEvent() {
+    return (
+        <div className={styles.pageWrapper}>
+            <Navbar />
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <h2>Edit *event_name*</h2>
+                </div>
+
+                <div className={styles.wrapper}>
+                    <form className={styles.form}>
+                        <div className={styles.left}>
+                            <label>
+                                Rename your event:
+                                <input type="text" placeholder="e.g. John's Birthday Bash" />
+                            </label>
+                            <label className={styles.dateInputWrapper}>
+                                Change the date:
+                                <input type="date" />
+                                <span className={styles.icon}></span>
+                            </label>
+                            <label>
+                                Change the location:
+                                <input type="text" placeholder="Enter location" />
+                            </label>
+                        </div>
+
+                        <div className={styles.right}>
+                            <label>
+                                Edit the note:
+                                <textarea placeholder="Event description..." />
+                            </label>
+                            <div className={styles.buttons}>
+                                <button className={`${buttonStyles.button} ${buttonStyles["button-primary"]}`}>
+                                    Cancel
+                                </button>
+
+                                <button className={`${buttonStyles.button} ${buttonStyles["button-primary"]}`}>
+                                    Save <br /> Changes
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
