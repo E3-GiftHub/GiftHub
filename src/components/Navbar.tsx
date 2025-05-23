24a3ece242dcffdb6075ef0731e3d34e1b614fd2
catiturcu <turcu_ecaterina@yahoo.com>
2025-05-23 19:46:02 +0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
       }`;
 
 
-      window.location.href = "http://localhost:3000/";
+      window.location.href = "/";
 
     }catch(err){
       console.error("Failure: ", err);
