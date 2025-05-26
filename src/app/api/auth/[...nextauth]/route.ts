10f54009d4c51dd8ac3fb53cd57a4147ec9ad83a
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-05-26 15:34:44 +0300

    Something about nextauth IDK

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,8 @@
 import { handlers } from "~/server/auth";
+import NextAuth from "next-auth";
+import { options } from "./options";
 
-export const { GET, POST } = handlers;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+const handler = NextAuth(options);
+
+export { handler as GET, handlers as POST };
