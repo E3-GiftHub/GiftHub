aafdfc0d945ddeb0bafd560c5a33d90be6a72e2f
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-05-27 14:29:41 +0300

    Something about nextauth IDK

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-// export { default } from "next-auth/middleware";
-//
-// export const config = {
-//   matcher:
-//   ["/home", "/inbox"]
-// };
+import { auth } from "~/app/api/auth/[...nextauth]/route";
+import {NextResponse} from "next/server";
+
+export default auth((req) => {
+  if(!req.auth){
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+});
+
+export const config = {
+  matcher: [
+    '/inbox',
+    '/home'
+  ],
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,80 @@
 import { handlers } from "~/server/auth";
 import NextAuth from "next-auth";
 import { options } from "./options";
+import { db } from "~/server/db";
+import * as bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const handler = NextAuth(options);
 
-export { handler as GET, handlers as POST };
+export const {
+  handlers: {GET, POST},
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials): Promise<any> {
+        if(!credentials?.email || !credentials?.password){
+          return null;
+        }
+
+        const user = await db.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if(!user || !user.password){
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        )
+
+        if(!passwordMatch){
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.username,
+          email: user.email
+        };
+      },
+    }),
+  ],
+
+  callbacks:{
+    async jwt({token, user}){
+      if(user){
+        token.id = user.id;
+        token.name = user.name;
+          token.email = user.email;
+      }
+      return token;
+    },
+
+    async session({session, token}){
+      if(token){
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+      }
+
+      return session;
+    },
+  },
+
+  pages:{
+    signIn: "/login",
+  },
+  secret: process.env.AUTH_SECRET,
+});
