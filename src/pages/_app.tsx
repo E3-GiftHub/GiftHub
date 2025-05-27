0cf76f885b3feaff590f0618109c3dc242dbddfe
P <petrubraha@gmail.com>
2025-05-27 13:20:40 +0300

    changed __app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
+import "~/styles/globals.css";
 import { type AppType } from "next/app";
 import { TRPCReactProvider } from "~/trpc/react";
+import { Geist } from "next/font/google";
+import Head from "next/head";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <TRPCReactProvider>
-      <Component {...pageProps} />
-    </TRPCReactProvider>
+    <>
+      <Head>
+        <title>GiftHub</title>
+        <meta
+          name="description"
+          content="For the love of events, parties, birthdays and more!"
+        />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <div className={geist.variable}>
+        <TRPCReactProvider>
+          <Component {...pageProps} />
+        </TRPCReactProvider>
+      </div>
+    </>
   );
 };
 
