38a26291be38897455b7bed059eb51b7d7cfac70
cosmingh03 <cosmin.ghiorghiu@gmail.com>
2025-05-30 19:02:39 +0300

    temporary cod for vercel fix

--- src/env.js
+++ src/env.js
@@ -60,3 +60,10 @@ export const env = createEnv({
    */
   emptyStringAsUndefined: true,
 });
+
+console.log("DEBUG ENV", {
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASS: process.env.SMTP_PASS,
+});
