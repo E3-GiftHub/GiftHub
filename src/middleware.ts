3b55f73d4f67bcb7a52e51f83b4ec91c8eebf945
P <petrubraha@gmail.com>
2025-06-01 19:13:48 +0300

    added more constraints

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,23 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  let cookie = request.cookies.get("next-auth.session-token");
+  const cookie = request.cookies.get("next-auth.session-token");
   if (!cookie) return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
-  matcher: ["/home", "/event", "/event-view"],
+  matcher: [
+    "/home",
+    "/inbox",
+    "/view-profile",
+    "/event-invitation",
+    "/event", // guest
+    "/wishlist", // guest
+    "/event-view", // planner
+    "/wishlist-create", // planner
+    "/contribution",
+    "/payment",
+    "/payment-failure",
+    "/payment-success",
+  ],
 };
