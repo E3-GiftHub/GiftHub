6345e010b890f579cfe449bbe13206256863007d
denizzz <denisturcanu84@gmail.com>
2025-06-04 09:57:37 +0300

    quick fix for react not defined (in sonar build), we can revert but it made 7 more passed tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -65,6 +65,7 @@ const config = {
     "ts-jest": {
       tsconfig: "tsconfig.test.json",
     },
+    React: "react",
   },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
