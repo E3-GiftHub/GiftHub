d69748a8d4be94ce3b1dd5b001c015b7997a4d3f
p <petrubraha@gmail.com>
2025-06-14 18:49:59 +0300

    fixed memory leak for deleting accounts

--- src/server/api/routers/profileManagenemt/user.ts
+++ src/server/api/routers/profileManagenemt/user.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { db } from "~/server/db";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { utapi } from "~/server/uploadthing";
 
 export const userRouter = createTRPCRouter({
   get: protectedProcedure.query(async ({ ctx }) => {
@@ -56,6 +57,16 @@ export const userRouter = createTRPCRouter({
     }),
 
   delete: protectedProcedure.mutation(async ({ ctx }) => {
+    const user = await db.user.findUnique({
+      where: { username: ctx.session.user.name! },
+      select: { pictureKey: true },
+    });
+
+    if (!user) return { success: false };
+
+    const { pictureKey } = user;
+    if (pictureKey) await utapi.deleteFiles(pictureKey);
+
     await db.user.delete({
       where: {
         username: ctx.session.user.name!,
